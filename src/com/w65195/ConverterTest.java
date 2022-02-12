a0cd0ef5ecddc97478b2eaa7be56c0b1b406e14b
kozga00 <krystianozga00@gmail.com>
2022-02-12 01:42:18 +0100

    added more test cases

--- src/com/w65195/ConverterTest.java
+++ src/com/w65195/ConverterTest.java
@@ -7,31 +7,34 @@ import java.text.DecimalFormat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConverterTest {
+  Converter converter = new Converter();
+  DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
   @Test
   public void shouldConvertFahrenheitToCelsius() {
-    Converter converter = new Converter();
-    DecimalFormat decimalFormat = new DecimalFormat("0.00");
+    assertEquals(decimalFormat.format(converter.fahrenheitToCelsius(0)), "-17.78");
+    assertEquals(decimalFormat.format(converter.fahrenheitToCelsius(10)), "-12.22");
     assertEquals(decimalFormat.format(converter.fahrenheitToCelsius(90)), "32.22");
   }
 
   @Test
   public void shouldConvertPoundsToKgs() {
-    Converter converter = new Converter();
-    DecimalFormat decimalFormat = new DecimalFormat("0.00");
+    assertEquals(decimalFormat.format(converter.poundsToKilograms(0)), "0.00");
+    assertEquals(decimalFormat.format(converter.poundsToKilograms(10)), "4.54");
     assertEquals(decimalFormat.format(converter.poundsToKilograms(90)), "40.82");
   }
 
   @Test
   public void shouldConvertFeetToMeters() {
-    Converter converter = new Converter();
-    DecimalFormat decimalFormat = new DecimalFormat("0.00");
+    assertEquals(decimalFormat.format(converter.feetToMeters(0)), "0.00");
+    assertEquals(decimalFormat.format(converter.feetToMeters(10)), "3.05");
     assertEquals(decimalFormat.format(converter.feetToMeters(90)), "27.43");
   }
 
   @Test
   public void shouldConvertMilesToKm() {
-    Converter converter = new Converter();
-    DecimalFormat decimalFormat = new DecimalFormat("0.00");
+    assertEquals(decimalFormat.format(converter.milesToKilometers(0)), "0.00");
+    assertEquals(decimalFormat.format(converter.milesToKilometers(10)), "16.09");
     assertEquals(decimalFormat.format(converter.milesToKilometers(90)), "144.84");
   }
 }
